a2ec40ba3267fbe28400d00fa84904a0095c7d4e
boazfr1 <boazfr1@gmail.com>
2024-05-14 21:43:19 +0300

    first commit

---
+++ build/CMakeFiles/monitor.dir/src/processor.cpp.o.d
@@ -0,0 +1,4 @@
+CMakeFiles/monitor.dir/src/processor.cpp.o: \
+ /home/hilma/development/c++/udacity-course/System-monitor-project/src/processor.cpp \
+ /usr/include/stdc-predef.h \
+ /home/hilma/development/c++/udacity-course/System-monitor-project/include/processor.h
